993cfa21d4229f5a38919b3981eee0a9271fe1e0
znehraks <znehraks@gmail.com>
2024-12-02 17:15:59 +0900

    chore: theme 예제 추가

--- src/components/atoms/Button.css.ts
+++ src/components/atoms/Button.css.ts
@@ -1,6 +1,8 @@
 import { recipe } from '@vanilla-extract/recipes';
+import { vars } from '../../theme.css';
 
 export const button = recipe({
+  base: { background: vars.color['--red-100'] },
   variants: {
     variant: {
       primary: {},
--- src/theme.css.ts
+++ src/theme.css.ts
@@ -1,7 +1,14 @@
 import { createTheme } from '@vanilla-extract/css';
 
 export const [themeClass, vars] = createTheme({
-  color: {},
+  color: {
+    '--red-100': '#FF0000',
+    '--red-200': '#FF0000',
+    '--blue-100': '#0000FF',
+    '--blue-200': '#0000FF',
+    '--green-100': '#00FF00',
+    '--green-200': '#00FF00',
+  },
   space: {},
   size: {},
   border: {},
